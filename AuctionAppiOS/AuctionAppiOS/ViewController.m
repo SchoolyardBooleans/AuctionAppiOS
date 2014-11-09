8db464c1543098e04c02fa47e8e497c50fbb3ab8
Jon Vazquez <jonmvazquez@gmail.com>
2014-11-09 15:06:40 -0800

    New basic project with salesforce api

---
+++ AuctionAppiOS/AuctionAppiOS/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AuctionAppiOS
+//
+//  Created by Jon Vazquez on 11/9/14.
+//  Copyright (c) 2014 Schoolyard Booleans. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
