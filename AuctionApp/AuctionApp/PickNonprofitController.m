1d2e4d9c081e9502d479f3371c342384c0e2f086
Jon Vazquez <jonmvazquez@gmail.com>
2014-12-02 11:16:57 -0800

    Added basic nonprofit view functionality
    Startup shows list of nonprofits

---
+++ AuctionApp/AuctionApp/PickNonprofitController.m
@@ -0,0 +1,60 @@
+//
+//  PickNonprofitController.m
+//  AuctionApp
+//
+//  Created by Jon Vazquez on 12/2/14.
+//  Copyright (c) 2014 Schoolyard Booleans. All rights reserved.
+//
+
+#import "PickNonprofitController.h"
+
+@interface PickNonprofitController ()
+
+@end
+
+@implementation PickNonprofitController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.model = [[PickNonprofitModel alloc] init];
+    self.nonprofits = [self.model getNonprofits];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.nonprofits count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *nonprofitCellID = @"PickNonprofitCellID";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:nonprofitCellID forIndexPath:indexPath];
+    
+    NonprofitInfo* org = [self.nonprofits objectAtIndex:indexPath.row];
+    
+    cell.textLabel.text = org.name;
+    
+    return cell;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
