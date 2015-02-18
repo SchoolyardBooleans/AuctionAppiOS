a8cf0b0434dc8b2c577d5bd2a8f963c470758801
James Fazio <james@thefazios.com>
2015-02-17 22:47:14 -0800

    Added code for when user clicks on link in email

--- AuctionApp/AuctionApp/AppDelegate.m
+++ AuctionApp/AuctionApp/AppDelegate.m
@@ -36,6 +36,17 @@
     return YES;
 }
 
+-(BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    NSString *code = [[[url query] componentsSeparatedByString:@"="] objectAtIndex:1];
+    
+    NSLog(@"URL query: %@", [url query]);
+    
+    [[NSUserDefaults standardUserDefaults] setValue:code forKey:@"code"];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+    
+    return YES;
+}
+
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
