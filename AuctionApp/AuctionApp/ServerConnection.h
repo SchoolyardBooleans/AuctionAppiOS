b6c91ce0ddc008695b3642f8ba11c7046040f631
James Fazio <james@thefazios.com>
2015-03-03 16:56:38 -0800

    Removed unused method

--- AuctionApp/AuctionApp/ServerConnection.h
+++ AuctionApp/AuctionApp/ServerConnection.h
@@ -1,15 +1,9 @@
-#ifndef Test_ServerConnection_h
-#define Test_ServerConnection_h
-
 #import <Foundation/Foundation.h>
 
 @interface ServerConnection : NSObject
 
-//+ (id)getJSONData:(NSString*)urlStr;
 + (void) httpGET:(NSString *) urlStr :(void (^)(id, NSString *)) callback;
 + (void) httpPOST:(NSString *) urlStr :(NSDictionary *) mapBody :(void (^)(id, NSString *)) callback;
 
 
 @end
-
-#endif
