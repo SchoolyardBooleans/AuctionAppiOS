906e6b5ff5dbdcd9134fa11dca64202eb8174eb4
James Fazio <james@thefazios.com>
2015-04-05 22:20:16 -0700

    Added global constants file, refactored method names, added comments to packages except viewControllers

---
+++ AuctionApp/AuctionApp/Constants.h
@@ -0,0 +1,13 @@
+//
+//  Constants.h
+//  AuctionApp
+//
+//  Created by James Fazio on 4/5/15.
+//  Copyright (c) 2015 Schoolyard Booleans. All rights reserved.
+//
+
+#ifndef AuctionApp_Constants_h
+#define AuctionApp_Constants_h
+
+
+#endif
---
+++ AuctionApp/AuctionApp/Constants.m
@@ -0,0 +1,9 @@
+//
+//  Constants.m
+//  AuctionApp
+//
+//  Created by James Fazio on 4/5/15.
+//  Copyright (c) 2015 Schoolyard Booleans. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
