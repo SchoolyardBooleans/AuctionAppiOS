4f1fb193b4b66ad97a4e69f399fea7680eb22e93
James Fazio <james@thefazios.com>
2015-04-27 13:55:35 -0700

    Changed uri for packaged version of app

--- AuctionApp/AuctionApp/Constants.m
+++ AuctionApp/AuctionApp/Constants.m
@@ -1,11 +1,11 @@
 #import "Constants.h"
 
-NSString *const REGISTER_URL = @"https://schooolyardbooleans-developer-edition.na16.force.com/public/services/apexrest/register";
-NSString *const BIDDER_URL = @"https://schooolyardbooleans-developer-edition.na16.force.com/public/services/apexrest/bidders";
-NSString *const LOGIN_URL = @"https://schooolyardbooleans-developer-edition.na16.force.com/public/services/apexrest/login";
-NSString *const ITEM_URL = @"https://schooolyardbooleans-developer-edition.na16.force.com/public/services/apexrest/auctionitems";
-NSString *const NONPROFIT_URL = @"https://schooolyardbooleans-developer-edition.na16.force.com/public/services/apexrest/nonprofits";
-NSString *const AUCTION_URL = @"https://schooolyardbooleans-developer-edition.na16.force.com/public/services/apexrest/auctions";
+NSString *const REGISTER_URL = @"https://schooolyardbooleans-developer-edition.na16.force.com/public/services/apexrest/bidfresh/register";
+NSString *const BIDDER_URL = @"https://schooolyardbooleans-developer-edition.na16.force.com/public/services/apexrest/bidfresh/bidders";
+NSString *const LOGIN_URL = @"https://schooolyardbooleans-developer-edition.na16.force.com/public/services/apexrest/bidfresh/login";
+NSString *const ITEM_URL = @"https://schooolyardbooleans-developer-edition.na16.force.com/public/services/apexrest/bidfresh/auctionitems";
+NSString *const NONPROFIT_URL = @"https://schooolyardbooleans-developer-edition.na16.force.com/public/services/apexrest/bidfresh/nonprofits";
+NSString *const AUCTION_URL = @"https://schooolyardbooleans-developer-edition.na16.force.com/public/services/apexrest/bidfresh/auctions";
 
 int const BEFORE = 0;
 int const IN_PROGRESS = 1;
